d74f071430ff855659078cbe489e271b2cdb60d1
0xShroud-GIT <0xshroud.me@gmail.com>
2026-08-22 00:44:55 +0200

    M0R: use production-testable PiP params factory

--- modules/partner-pip/android/src/main/java/com/partnerscreen/pip/PartnerPipModule.kt
+++ modules/partner-pip/android/src/main/java/com/partnerscreen/pip/PartnerPipModule.kt
@@ -1,9 +1,7 @@
 package com.partnerscreen.pip
 
-import android.app.PictureInPictureParams
 import android.content.Intent
 import android.os.Build
-import android.util.Rational
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
@@ -17,7 +15,7 @@ class PartnerPipModule : Module() {
     AsyncFunction("enterPip") { width: Int, height: Int ->
       val activity = appContext.currentActivity ?: return@AsyncFunction false
       if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return@AsyncFunction false
-      val params = try { pictureParams(width, height) } catch (_: Exception) { return@AsyncFunction false }
+      val params = try { PipParamsFactory.create(width, height) } catch (_: Exception) { return@AsyncFunction false }
       if (activity.isInPictureInPictureMode) {
         try { activity.setPictureInPictureParams(params) } catch (_: Exception) {}
         wasInPip = true
@@ -41,7 +39,7 @@ class PartnerPipModule : Module() {
       if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return@AsyncFunction false
       if (!activity.isInPictureInPictureMode) return@AsyncFunction false
       try {
-        activity.setPictureInPictureParams(pictureParams(width, height))
+        activity.setPictureInPictureParams(PipParamsFactory.create(width, height))
         true
       } catch (_: Exception) {
         false
@@ -108,13 +106,5 @@ class PartnerPipModule : Module() {
         sendEvent("onPipModeChanged", mapOf("isInPictureInPictureMode" to false))
       }
     }
-  }
-
-  private fun pictureParams(width: Int, height: Int): PictureInPictureParams {
-    val w = width.coerceIn(1, 1920)
-    val h = height.coerceIn(1, 1920)
-    return PictureInPictureParams.Builder()
-      .setAspectRatio(Rational(w, h))
-      .build()
   }
 }
