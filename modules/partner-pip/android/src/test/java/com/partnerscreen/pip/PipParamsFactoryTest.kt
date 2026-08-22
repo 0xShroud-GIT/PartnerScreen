919b4cb88c0ba1ba73d7fc468ccbeac3929c6727
0xShroud-GIT <0xshroud.me@gmail.com>
2026-08-22 02:40:24 +0200

    Restore Runtime Lab PiP tests

--- modules/partner-pip/android/src/test/java/com/partnerscreen/pip/PipParamsFactoryTest.kt
+++ modules/partner-pip/android/src/test/java/com/partnerscreen/pip/PipParamsFactoryTest.kt
@@ -1,1 +1,29 @@
-fatal: path 'modules/partner-pip/android/src/test/java/com/partnerscreen/pip/PipParamsFactoryTest.kt' exists on disk, but not in 'tmp/pr11'
+package com.partnerscreen.pip
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [26, 35])
+class PipParamsFactoryTest {
+  @Test
+  fun actualVideoGeometryBecomesPictureInPictureAspectRatio() {
+    val landscape = PipParamsFactory.create(1280, 720).aspectRatio
+    assertEquals(16, landscape?.numerator)
+    assertEquals(9, landscape?.denominator)
+
+    val portrait = PipParamsFactory.create(720, 1280).aspectRatio
+    assertEquals(9, portrait?.numerator)
+    assertEquals(16, portrait?.denominator)
+  }
+
+  @Test
+  fun geometryIsBoundedBeforeCreatingAndroidParams() {
+    val params = PipParamsFactory.create(9_999, 1).aspectRatio
+    assertEquals(1920, params?.numerator)
+    assertEquals(1, params?.denominator)
+  }
+}
